dbe6ad6bf6c38cdc80f0f67c6bbd2b91883e965c
Josh Williams <23joshwilliams@gmail.com>
2016-06-11 18:58:07 -0700

    Added Stripe Payment functionality

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,7 @@
 $(function() {
 
+  Stripe.setPublishableKey('pk_test_aCjBN943RboHFpotz7pRKNEp');
+
   $('#search').keyup(function() {
 
     var search_term = $(this).val();
@@ -68,4 +70,45 @@ $(function() {
     $('#priceValue').val(priceValue.toFixed(2));
     $('#total').html(quantity);
   });
+
+  function stripeResponseHandler(status, response) {
+  // Grab the form:
+  var $form = $('#payment-form');
+
+  if (response.error) { // Problem!
+
+    // Show the errors on the form:
+    $form.find('.payment-errors').text(response.error.message);
+    $form.find('.submit').prop('disabled', false); // Re-enable submission
+
+  } else { // Token was created!
+
+    // Get the token ID:
+    var token = response.id;
+
+    // Insert the token ID into the form so it gets submitted to the server:
+    $form.append($('<input type="hidden" name="stripeToken">').val(token));
+
+    // Submit the form:
+    $form.get(0).submit();
+  }
+};
+
+$(function() {
+  var $form = $('#payment-form');
+  $form.submit(function(event) {
+    // Disable the submit button to prevent repeated clicks:
+    $form.find('.submit').prop('disabled', true);
+
+    // Request a token from Stripe:
+    Stripe.card.createToken($form, stripeResponseHandler);
+
+    // Prevent the form from being submitted:
+    return false;
+  });
+});
+
+
+
+
 });
--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,9 @@ var router = require('express').Router();
 var User = require('../models/user');
 var Product = require('../models/product');
 var Cart = require('../models/cart');
+var async = require('async');
+
+var stripe = require('stripe')('sk_test_eVElTtCqeteawqIxgVcrDkek');
 
 function paginate(req, res, next) {
   var perPage = 9;
@@ -149,6 +152,22 @@ router.get('/product/:id', function(req, res, next) {
       product: product
     });
   });
+});
+
+router.post('/payment', function(req, res, next) {
+  var stripeToken = req.body.stripeToken;
+  var currentCharges = Math.round(req.body.stripeMoney * 100);
+  stripe.customers.create({
+    source: stripeToken,
+  }).then(function (customer) {
+    return stripe.charges.create({
+      amount: currentCharges,
+      currency: 'usd',
+      customer: customer.id
+    });
+  });
+
+  res.redirect('/profile');
 });
 
 module.exports = router;
