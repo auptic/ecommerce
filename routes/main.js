0da39beed361a5abbeb7c11863a3dcd8918c2558
Josh Williams <23joshwilliams@gmail.com>
2016-06-12 18:06:40 -0700

    Completed all required functionality

--- routes/main.js
+++ routes/main.js
@@ -165,9 +165,38 @@ router.post('/payment', function(req, res, next) {
       currency: 'usd',
       customer: customer.id
     });
+  }).then(function (charge) {
+    async.waterfall([
+      function (callback) {
+        Cart.findOne({ owner: req.user._id }, function(err, cart) {
+          callback(err, cart);
+        });
+      },
+      function (cart, callback) {
+        User.findOne({ _id: req.user._id }, function (err, user) {
+          if (user) {
+            for (var i = 0; i < cart.items.length; i++) {
+              user.history.push({
+                item: cart.items[i].item,
+                paid: cart.items[i].price
+              });
+            }
+            user.save(function (err, user) {
+              if (err) return next(err);
+              callback(err, user);
+            });
+          }
+        });
+      },
+      function (user) {
+        Cart.update({ owner: user._id }, { $set: { items: [], total: 0}}, function (err, updated) {
+          if (updated) {
+            res.redirect('/profile');
+          }
+        });
+      }
+    ]);
   });
-
-  res.redirect('/profile');
 });
 
 module.exports = router;
